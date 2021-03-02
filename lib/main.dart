a47af3e332481be472bc19839b2d936c8827fd8d
danhausen <danihausen@live.com>
2021-03-02 19:33:55 -0300

    main

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,23 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Scaffold());
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: SafeArea(
+          child: Container(
+            padding:
+                EdgeInsets.all(20), //Used to change container content position
+            margin: EdgeInsets.all(
+                50), //Used to change the position of the container itself
+            child: Text(
+                'Dan'), //A container as a single child widget can contain only one child
+            color: Colors.white,
+            height: 100.0,
+            width: 100.0,
+          ),
+        ),
+      ),
+    );
   }
 }
